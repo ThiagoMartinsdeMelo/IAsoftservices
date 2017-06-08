d643e11bfa90ef3792bb9e80abe51c817b95bef5
Thiago Martins <thiago@vras.info>
2017-06-08 19:22:43 -0300

    dump

--- core/Controller.php
+++ core/Controller.php
@@ -11,15 +11,6 @@ class controller {
 		$this->config = $cfg->getConfig();
 	}
 	
-	public function loadView($viewName, $viewData = array()) {
-		extract($viewData);
-		include 'views/'.$viewName.'.php';
-	}
-
-	public function loadTemplate($viewName, $viewData = array()) {
-		include 'views/templates/'.$this->config['site_template'].'.php';
-	}
-
 	public function loadTemplateInPainel($viewName, $viewData = array()) {
 		include 'views/painel.php';
 	}
